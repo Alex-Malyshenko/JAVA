747ad22c13ceaaf1dcff778076d7edf84804ebab
Alex-Malyshenko <alis_mal@mail.ru>
2023-04-16 23:36:56 +0300

    End Task_home_2

---
+++ Task_home_2.java
@@ -0,0 +1,20 @@
+//Вывести все простые числа от 1 до 1000
+
+public class Task_home_2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 1000; i++) {
+            boolean flag = true;
+            for (int j = 2; j < i; j++) {
+                if (i%j==0) {
+                    flag = false;
+                }
+            }
+            
+            if (flag==true) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    
+}
