3b554401a819e6467e0d9f0821221086644ae1af
Alex-Malyshenko <alis_mal@mail.ru>
2023-04-26 20:31:43 +0300

    End Task_home_6

--- Task_home_6.java
+++ Task_home_6.java
@@ -4,16 +4,16 @@ public class Task_home_6 {
     
     public class sortArray<T> {}
 
-    public static void quickSort(int[] sortArr, int low, int high) {
+    public static void sort(int[] sortArr, int low, int high) {
         if (sortArr.length == 0 || low >= high) return;
 
         int middle = low + (high - low) / 2;
-        int border = sortArr[middle];
+        int elemMid = sortArr[middle];
 
         int i = low, j = high;
         while (i <= j) {
-            while (sortArr[i] < border) i++;
-            while (sortArr[j] > border) j--;
+            while (sortArr[i] < elemMid) i++;
+            while (sortArr[j] > elemMid) j--;
             if (i <= j) {
                 int swap = sortArr[i];
                 sortArr[i] = sortArr[j];
@@ -23,15 +23,15 @@ public class Task_home_6 {
             }
         }
     
-        if (low < j) quickSort(sortArr, low, j);
-        if (high > i) quickSort(sortArr, i, high);
+        if (low < j) sort(sortArr, low, j);
+        if (high > i) sort(sortArr, i, high);
     }
     
     public static void main(String args[]) {
-        int[] sortArr = {-9, 4, 8,14, 54, 44, -8, 0, 1, 11, 78, 3, -11};
-        quickSort(sortArr, 0, sortArr.length - 1);
-        for(int i = 0; i < sortArr.length; i++){
-            System.out.print(sortArr[i] + " ");
+        int[] mass = {-9, 4, 8,14, 54, 44, -8, 0, 1, 11, 78, 3, -11};
+        sort(mass, 0, mass.length - 1);
+        for(int i = 0; i < mass.length; i++){
+            System.out.print(mass[i] + " ");
         }
     }
 }
