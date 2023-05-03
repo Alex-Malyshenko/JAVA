4e22a61fec71fbd6c7f95fcce63e553ddf8336d3
Alex-Malyshenko <alis_mal@mail.ru>
2023-05-03 21:25:37 +0300

    End Task_home_14

--- Task_home_13.java
+++ Task_home_13.java
@@ -1,34 +1,33 @@
 // Реализовать алгоритм пирамидальной сортировки (HeapSort).
 
 public class Task_home_13 {
-   
+
     public static int LEFT(int i) {
         return (2*i + 1);
     }
- 
-   
+
     public static int RIGHT(int i) {
         return (2*i + 2);
     }
- 
+
     public static void swap(int[] arr, int i, int j)
     {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
- 
+
     public static void heapify(int[] arr, int i, int size)
     {
         int left = LEFT(i);
         int right = RIGHT(i);
- 
+
         int largest = i;
- 
+
         if (left < size && arr[left] > arr[i]) {
             largest = left;
         }
- 
+
         if (right < size && arr[right] > arr[largest]) {
             largest = right;
         }
@@ -38,7 +37,7 @@ public class Task_home_13 {
             heapify(arr, largest, size);
         }
     }
- 
+
     public static int pop(int[] arr, int size)
     {
         if (size <= 0) {
@@ -47,10 +46,9 @@ public class Task_home_13 {
         int top = arr[0];
         arr[0] = arr[size-1];
         heapify(arr, 0, size - 1);
- 
         return top;
     }
- 
+
     public static void heapsort(int[] arr)
     {
         int n = arr.length;
@@ -58,7 +56,7 @@ public class Task_home_13 {
         while (i >= 0) {
             heapify(arr, i--, n);
         }
- 
+
         while (n > 0)
         {
             arr[n - 1] = pop(arr, n);
